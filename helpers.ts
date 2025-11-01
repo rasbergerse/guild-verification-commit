f86ea28ff11de1c36b08f42051792cdc64bade28
RTE <111799086+rasbergerse@users.noreply.github.com>
2025-11-01 20:28:47 +0300

    Fix race condition in async operations

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-01T17:28:43.312Z
-Commit: Implement user profile customization
+Timestamp: 2025-11-01T17:28:47.674Z
+Commit: Fix race condition in async operations
