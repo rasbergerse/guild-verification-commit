20e13f40d431d9be9c2bbbae101841f31fd82175
RTE <111799086+rasbergerse@users.noreply.github.com>
2025-11-01 20:29:06 +0300

    Fix CSS styling bug on mobile devices

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-01T17:28:59.087Z
-Commit: Correct typo in configuration file
+Timestamp: 2025-11-01T17:29:06.875Z
+Commit: Fix CSS styling bug on mobile devices
