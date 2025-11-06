85e9a1a27f7842e1d78db8da6956e932f99d38de
RTE <111799086+rasbergerse@users.noreply.github.com>
2025-11-06 03:15:14 +0300

    Fix broken links in documentation

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-06T00:15:07.762Z
-Commit: Patch API endpoint error handling
+Timestamp: 2025-11-06T00:15:13.153Z
+Commit: Fix broken links in documentation
