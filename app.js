008f0d0b8dc71239d1bb8a6c2e601a98accc2fef
RTE <111799086+rasbergerse@users.noreply.github.com>
2025-11-06 03:18:22 +0300

    Add multi-language support

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-06T00:18:19.631Z
-Commit: Resolve timezone issues in date handling
+Timestamp: 2025-11-06T00:18:21.109Z
+Commit: Add multi-language support
