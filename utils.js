0f471a579276df1bbae9fff9325c29fe1cfbfe7d
RTE <111799086+rasbergerse@users.noreply.github.com>
2025-11-06 03:25:09 +0300

    Fix memory leak in event handlers

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-06T00:25:06.408Z
-Commit: Patch security vulnerability in authentication
+Timestamp: 2025-11-06T00:25:07.827Z
+Commit: Fix memory leak in event handlers
