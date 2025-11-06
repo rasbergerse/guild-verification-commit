b60fca29eed826d5ab9f1b59436578a3ab0c2ac8
RTE <111799086+rasbergerse@users.noreply.github.com>
2025-11-06 03:19:21 +0300

    Resolve null pointer exception in user service

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-06T00:19:18.454Z
-Commit: Add multi-language support
+Timestamp: 2025-11-06T00:19:19.986Z
+Commit: Resolve null pointer exception in user service
