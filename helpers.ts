9c4d250f4fe6fe53925c7cdd5cb0a35725a954df
RTE <111799086+rasbergerse@users.noreply.github.com>
2025-11-06 03:20:46 +0300

    Fix broken links in documentation

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-06T00:20:37.302Z
-Commit: Resolve timezone issues in date handling
+Timestamp: 2025-11-06T00:20:45.661Z
+Commit: Fix broken links in documentation
