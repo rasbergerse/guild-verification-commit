aa89499b9e4939634ff072c9497cdc54db6b7ef0
RTE <111799086+rasbergerse@users.noreply.github.com>
2025-11-06 03:14:40 +0300

    Fix CSS styling bug on mobile devices

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-06T00:14:30.564Z
-Commit: Introduce real-time notifications
+Timestamp: 2025-11-06T00:14:39.501Z
+Commit: Fix CSS styling bug on mobile devices
