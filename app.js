bce6c19bf4170dc62d8fb2b1076d12914a07a2cd
RTE <111799086+rasbergerse@users.noreply.github.com>
2025-11-06 03:19:00 +0300

    Resolve null pointer exception in user service

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-06T00:18:21.109Z
-Commit: Add multi-language support
+Timestamp: 2025-11-06T00:18:59.651Z
+Commit: Resolve null pointer exception in user service
