abcd794f04b306b597893360ff8aa773d56c884b
RTE <111799086+rasbergerse@users.noreply.github.com>
2025-11-06 03:14:08 +0300

    Fix CSS styling bug on mobile devices

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-06T00:14:05.251Z
-Commit: Add export functionality for reports
+Timestamp: 2025-11-06T00:14:06.759Z
+Commit: Fix CSS styling bug on mobile devices
