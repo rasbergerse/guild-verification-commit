b461e7d45b104919a6e383b9b43e55410d36d0d9
RTE <111799086+rasbergerse@users.noreply.github.com>
2025-11-06 03:19:18 +0300

    Fix memory leak in event handlers

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-06T00:19:03.657Z
-Commit: Fix CSS styling bug on mobile devices
+Timestamp: 2025-11-06T00:19:17.085Z
+Commit: Fix memory leak in event handlers
