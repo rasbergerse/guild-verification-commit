41897da830c677be8a46c3690d74f3ce40411445
RTE <111799086+rasbergerse@users.noreply.github.com>
2025-11-06 03:18:01 +0300

    Patch security vulnerability in authentication

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-06T00:16:31.419Z
-Commit: Fix memory leak in event handlers
+Timestamp: 2025-11-06T00:18:00.226Z
+Commit: Patch security vulnerability in authentication
