41f8277351933561d30ba33808ec9d583e8947ca
RTE <111799086+rasbergerse@users.noreply.github.com>
2025-11-06 03:19:04 +0300

    Fix CSS styling bug on mobile devices

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-06T00:18:41.642Z
-Commit: Patch security vulnerability in authentication
+Timestamp: 2025-11-06T00:19:03.657Z
+Commit: Fix CSS styling bug on mobile devices
