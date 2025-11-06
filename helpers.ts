13ecbdaa9c52b9d6920f86f8c9d7aada1b327af1
RTE <111799086+rasbergerse@users.noreply.github.com>
2025-11-06 03:20:34 +0300

    Patch API endpoint error handling

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-06T00:19:58.129Z
-Commit: Implement user profile customization
+Timestamp: 2025-11-06T00:20:33.192Z
+Commit: Patch API endpoint error handling
