c7d5ed5f6c029fc353075366931d1ebcd0f532f1
RTE <111799086+rasbergerse@users.noreply.github.com>
2025-11-06 03:20:38 +0300

    Resolve timezone issues in date handling

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-06T00:20:33.192Z
-Commit: Patch API endpoint error handling
+Timestamp: 2025-11-06T00:20:37.302Z
+Commit: Resolve timezone issues in date handling
